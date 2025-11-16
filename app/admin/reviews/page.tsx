b2c3b458f79b9d7ab82998c3c8ee33eb9eab2142
sk fahim hossain <fahimhossainjob1@gmail.com>
2025-11-17 00:35:28 +0600

    sdfd

--- app/admin/reviews/page.tsx
+++ app/admin/reviews/page.tsx
@@ -1,9 +1,5 @@
 "use client"
 
-import { useEffect, useState } from "react";
-
-
-
 export default function SkillPage() {
 
 
--- app/admin/upload/page.tsx
+++ app/admin/upload/page.tsx
@@ -1,9 +1,6 @@
 "use client"
 
 
-
-
-
 export default function UploadPage() {
     
     return (
